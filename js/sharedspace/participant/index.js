2551862eaf45391f7fda265930ccc491f01071b0
Salvador de la Puente González <salva@unoyunodiez.com>
2017-09-17 20:12:08 +0200

    Adding connection randomess and explanation.

--- js/sharedspace/participant/index.js
+++ js/sharedspace/participant/index.js
@@ -7,6 +7,59 @@ const bind = utils.bind;
 const log = utils.debug('sharedspace:participant:log');
 const warn = utils.debug('sharedspace:participant:warn');
 
+function wait(time) {
+  return new Promise(fulfill => setTimeout(fulfill, time));
+}
+
+/**
+ * The Participant class represents the participation model.
+ *
+ * This model tries to guarantee the entering order for all participants to be
+ * the same and so, the problems translates to keeping a list of participants
+ * the same for all the participants.
+ *
+ * To that end, the source of trust is centralized into the "host" participant.
+ *
+ * For choosing the host, a participants starts with "unknown" role. As soon as
+ * it discovers another participant, it creates a two item participant list
+ * with itself and the other participant and broadcast this list along with its
+ * enter time.
+ *
+ * Eventually, it will receive another list from the other participant. Then,
+ * the list with an older enter time becomes the trusted one. If this list was
+ * that coming from the other participant, the role of this participant will be
+ * "guest", otherwise it will be "host".
+ *
+ * If a participant becomes the "host", it will start broadcasting the list
+ * periodically.
+ *
+ * Here is a risk of creating split networks (i.e. severals hosts) when a lot
+ * of peers join simulataneously. To mitigate this effect, connecting is delayed
+ * randomly from 1 to 3 seconds.
+ *
+ * The "host" will send the participant list periodically and will ignore
+ * any other list. The "guests" will only accept lists coming from the "host"
+ * and the "unknown" participants joining later will choose the "host" list
+ * as reference as described before.
+ *
+ * Notice that entering and leaving events also happen along with list updates
+ * and it is not guranteed that they happen in any specific order.
+ *
+ * When some participant leaves, it is not removed from the list but marked
+ * as absent (nullified right now). It could be the participant leaving is
+ * the current host. Then a takeover happens.
+ *
+ * If the host is leaving, all the "guests" will remove the host from their
+ * lists. Since their lists are the same, the next host (the next non-null
+ * participant) must be the same. This participant will also upgrade its role
+ * to "host" and will start broadcasting its list periodically.
+ *
+ * Peer to peer communications are not guaranteed to reach the destination.
+ * The same happens with the order of reception of sent messages. This is
+ * enough right now but it introduces a high risk of split networks if there is
+ * a lot of participants joining simultaneously. **It is recommended to join one
+ * by one to avoid the risk of a network split.**
+ */
 class Participant extends EventTarget {
   constructor(room, { id, stream, provider }) {
     super();
@@ -20,7 +73,8 @@ class Participant extends EventTarget {
   }
 
   connect() {
-    return this._rtc.connect()
+    return wait(1000 + Math.random(2000)) // XXX: see explanation above.
+    .then(() => this._rtc.connect())
     .then(() => {
       this._enterTime = Date.now();
       this._list = window.list = new GuestList(this._enterTime, [this.me]);
@@ -56,13 +110,15 @@ class Participant extends EventTarget {
 
   /*
    * Becomes the host if the current host is leaving and it is the next.
+   * It only removes the participant if it is the host, to properly calculate
+   * the next host.
    */
-  _takeover(leavingGuest) {
-    const hostLeft = leavingGuest === this._list.host();
+  _takeover(participant) {
+    const isHost = participant === this._list.host();
     const meIsNext = this.me === this._list.nextHost();
-    if (hostLeft) {
+    if (isHost) {
       log('host is leaving, be prepared for the takeover');
-      this._list.remove(leavingGuest);
+      this._list.remove(participant);
       if (meIsNext) {
         log('taking over');
         this._setRole('host');
@@ -118,7 +174,7 @@ class Participant extends EventTarget {
     setTimeout(() => {
       this._broadcastList();
       this._heartBeatList();
-    }, 5000 + Math.random(1000));
+    }, 3000);
   }
 
   /**
