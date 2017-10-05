45143dc0518e9537bfd06fda89dcbac9376da1aa
Salvador de la Puente González <salva@unoyunodiez.com>
2017-10-05 19:59:02 +0200

    position-around component tested

---
+++ tests/components/position-around.test.js
@@ -0,0 +1,79 @@
+const helpers = require('../helpers');
+
+suite.only('position-around component', () => {
+  let entity;
+
+  suiteSetup(() => {
+    return helpers.entityFactory()
+    .then(newEntity => {
+      entity = newEntity;
+      sinon.spy(entity, 'setAttribute');
+    });
+  });
+
+  teardown(() => {
+    entity.setAttribute.reset();
+  });
+
+  function forCircle([ x, y, z, ], r, h) {
+    const s = Math.sin(Math.PI/4);
+    return [
+      { x: r + x, y: y + h, z },
+      { x: -(r + x), y: y + h, z: -z },
+      { x, y: y + h, z: r + z },
+      { x: -x, y: y + h, z: -(r + z) },
+      { x: s * r + x, y: y + h, z: s * r + z },
+      { x: -(s * r + x), y: y + h, z: -(s * r + z) },
+      { x: -s * r + x, y: y + h, z: s * r + z },
+      { x: -(-s * r + x), y: y + h, z: -(s * r + z) }
+    ];
+  }
+
+  suite('with default configuration', () => {
+    const testSet = forCircle([0, 0, 0], 1.1, 1.6);
+
+    testSet.forEach((expectedPosition, index) => {
+      test(`changes an entity's position to be arranged around a circle (position ${index+1})`, () => {
+        entity.setAttribute('position-around', { position: index + 1 });
+        assertWithThreshold(
+          entity.setAttribute.getCall(1).args,
+          expectedPosition
+        );
+      });
+    });
+
+  });
+
+  suite('with modified parameters', () => {
+    const center = { x: 1, y: -2, z: 3 };
+    const radius = 0.5;
+    const height = -2;
+    const testSet = forCircle([center.x, center.y, center.z], radius, height);
+
+    setup(() => {
+      entity.setAttribute('position-around', { center, radius, height });
+      entity.setAttribute.reset();
+    });
+
+    testSet.forEach((expectedPosition, index) => {
+      test(`changes an entity's position to be arranged around a circle (position ${index+1})`, () => {
+        entity.setAttribute('position-around', { position: index + 1 });
+        assertWithThreshold(
+          entity.setAttribute.getCall(1).args,
+          expectedPosition
+        );
+      });
+    });
+
+  });
+
+  function assertWithThreshold(target, expected) {
+    const THRESHOLD = 0.001;
+    const [ attr, { x, y, z } ] = target;
+    assert.equal(attr, 'position');
+    assert.isBelow(Math.abs(x - expected.x), THRESHOLD, 'x not similar enough');
+    assert.isBelow(Math.abs(y - expected.y), THRESHOLD, 'y not similar enough');
+    assert.isBelow(Math.abs(z - expected.z), THRESHOLD, 'z not similar enough');
+  }
+
+});
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,7 @@
 require('babel-polyfill');
 require('aframe');
 require('../src');
-require('./components/avatars/avatars.test.js');
-require('./components/avatars/entity-observer.test.js');
-require('./components/avatars/scene-tree.test.js');
+require('./components/avatars/avatars.test');
+require('./components/avatars/entity-observer.test');
+require('./components/avatars/scene-tree.test');
+require('./components/position-around.test');
